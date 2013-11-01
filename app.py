0ec302549e808055afafd1f03f2db34c90229a6a
jake <jake@coffshire.com>
2013-10-31 20:25:20 -0500

    This is way easier than flask + sqlalchemy.

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
 import json
 import falcon
+import peewee
 
-class JSONResource(object):
-    def on_get(self, request, response):
-        json_data = {"message": "Hello, world!"}
-        response.body = json.dumps(json_data)
+
+db = peewee.SqliteDatabase('users.db')
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    username = peewee.CharField()
+    email = peewee.CharField()
+
+    def __str__(self):
+        return json.dumps({
+            'username': self.username,
+            'email': self.email
+        })
+
+
+class UsersResource(object):
+    def on_get(self, request, response, user_id=None):
+        if user_id is None:
+            response.body = json.dumps([u.username for u in User.select()])
+        else:
+            user = User.get(username=user_id)
+            response.body = str(user)
+
 
 app = falcon.API()
 
-json_resource = JSONResource()
+users_resource = UsersResource()
 
-app.add_route("/json", json_resource)
+app.add_route("/users", users_resource)
+app.add_route("/users/{user_id}", users_resource)
 
 # For debugging. A performant way to launch is gunicorn.
 if __name__ == "__main__":
